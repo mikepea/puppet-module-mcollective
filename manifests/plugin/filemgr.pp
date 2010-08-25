92c0bc68ce56325421b0d2c0505691e4506a4c00
Mike Pountney <Mike.Pountney@gmail.com>
2010-08-25 11:39:37 +0100

    fix location of filemgr.rb

--- manifests/plugin/filemgr.pp
+++ manifests/plugin/filemgr.pp
@@ -1,5 +1,5 @@
 
 class mcollective::plugin::filemgr {
-    mcollective::plugin_file { "filemgr.rb": source => "agent/filemgr/filemgr.rb" }
+    mcollective::plugin_file { "agent/filemgr.rb": source => "agent/filemgr/filemgr.rb" }
 }
 
